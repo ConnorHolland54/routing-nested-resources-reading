4f5f559a8b692a0f7bf7707db0e0b7bb699d7455
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-06 11:40:42 -0600

    commit tree b62a44e2e3ececb68b80ad9bfab8d19f7af7b768

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,18 @@
 class AuthorsController < ApplicationController
 
+  def index
+    if params[:author_id]
+      @posts = Author.find(params[:author_id]).posts
+    else
+      @posts = Post.all
+    end
+  end
+
+
   def show
     @author = Author.find(params[:id])
   end
+
+
 
 end
